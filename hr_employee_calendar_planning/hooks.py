5b2270c999ecdd5ed4fb3f2546c5027f109ff14d
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-03-21 14:44:13 +0100

    simplify init hook. Fix tests

--- hr_employee_calendar_planning/hooks.py
+++ hr_employee_calendar_planning/hooks.py
@@ -12,49 +12,14 @@ def post_init_hook(cr, registry, employees=None):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
         if not employees:
-            employees = env['hr.employee'].search([])
-        calendars = employees.mapped('resource_calendar_id')
-        calendar_obj = env['resource.calendar']
-        line_obj = env['resource.calendar.attendance']
-        groups = line_obj.read_group(
-            [('calendar_id', 'in', calendars.ids)],
-            ['calendar_id', 'date_from', 'date_to'],
-            ['calendar_id', 'date_from:day', 'date_to:day'],
-            lazy=False,
-        )
-        calendar_mapping = defaultdict(list)
-        for group in groups:
-            calendar = calendar_obj.browse(group['calendar_id'][0])
-            lines = line_obj.search(group['__domain'])
-            if len(calendar.attendance_ids) == len(lines):
-                # Don't alter calendar, as it's the same
-                new_calendar = calendar
-            else:
-                name = calendar.name + " %s-%s" % (
-                    lines[0].date_from, lines[0].date_to,
-                )
-                attendances = []
-                for line in lines:
-                    attendances.append((0, 0, {
-                        'name': line.name,
-                        'dayofweek': line.dayofweek,
-                        'hour_from': line.hour_from,
-                        'hour_to': line.hour_to,
-                    }))
-                new_calendar = calendar_obj.create({
-                    'name': name,
-                    'attendance_ids': attendances,
-                })
-            calendar_mapping[calendar].append(
-                (lines[0].date_from, lines[0].date_to, new_calendar),
-            )
+            employees = env['hr.employee'].search([(
+                'resource_calendar_id', '!=', False,
+            )])
         for employee in employees:
-            calendar_lines = []
-            for data in calendar_mapping[employee.resource_calendar_id]:
-                calendar_lines.append((0, 0, {
-                    'date_start': data[0],
-                    'date_end': data[1],
-                    'calendar_id': data[2].id,
-                }))
+            calendar_lines = [(0, 0, {
+                'date_start': False,
+                'date_end': False,
+                'calendar_id': employee.resource_calendar_id.id,
+            })]
             employee.resource_calendar_id = False
             employee.calendar_ids = calendar_lines
