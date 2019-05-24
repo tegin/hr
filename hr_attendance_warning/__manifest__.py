e9de176e3026a3d425ae6012ba6c061801ab8e19
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-05-24 14:32:10 +0200

    [11.0][IMP] Systray Widget

--- hr_attendance_warning/__manifest__.py
+++ hr_attendance_warning/__manifest__.py
@@ -19,10 +19,16 @@
     ],
     'data': [
         'data/ir_sequence_data.xml',
+        'data/ir_config_parameter_data.xml',
         'data/check_issues_cron.xml',
+        'security/hr_warnings_security.xml',
         'security/ir.model.access.csv',
+        'views/assets_backend.xml',
         'views/hr_attendance_warning.xml',
         'views/resource_calendar_attendance.xml',
         'wizards/hr_attendance_warning_solve.xml',
+    ],
+    'qweb': [
+        'static/src/xml/systray.xml',
     ],
 }
