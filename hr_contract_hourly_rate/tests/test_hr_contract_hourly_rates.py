26e15d0640a5778c60c7dac8af44d06b905876a0
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-05-28 12:13:06 +0200

    [11.0][FIX] hr_contract_hourly_rate: Split readme

--- hr_contract_hourly_rate/tests/test_hr_contract_hourly_rates.py
+++ hr_contract_hourly_rate/tests/test_hr_contract_hourly_rates.py
@@ -1,22 +1,6 @@
-##############################################################################
-#
-#    Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright (C) 2014 Savoir-faire Linux. All Rights Reserved.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 
 from odoo.tests.common import TransactionCase
 from odoo import exceptions
@@ -170,6 +154,9 @@ class TestContractHourlyRate(TransactionCase):
                 dates[0], dates[1], job_id=self.job_3_id.id, main_job=False)
 
             self.assertTrue(res == 25)
+
+        self.contract_id.contract_job_ids[0]._get_current_hourly_rate()
+        self.assertEqual(self.contract_id.contract_job_ids[0].hourly_rate, 0)
 
     def test_get_job_hourly_rate_main_job(self):
         """
