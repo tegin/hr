7110b7456e2fc7a093bf79cbf9d475102e9fad6b
Enric Tobella <etobella@creublanca.es>
2020-05-19 19:39:12 +0200

    [IMP] modification_tracking: Check if time was manually changed

--- hr_attendance_modification_tracking/models/hr_attendance.py
+++ hr_attendance_modification_tracking/models/hr_attendance.py
@@ -1,7 +1,8 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import timedelta
 
 
 class HrAttendance(models.Model):
@@ -17,3 +18,25 @@ class HrAttendance(models.Model):
     check_out = fields.Datetime(
         track_visibility='onchange'
     )
+    time_changed_manually = fields.Boolean(
+        string="Time changed",
+        readonly=True,
+        compute='_compute_time_changed_manually',
+        store=True,
+    )
+
+    @api.one
+    @api.depends('message_ids.tracking_value_ids')
+    def _compute_time_changed_manually(self):
+        if not self.time_changed_manually:
+            for tracking in self.message_ids.mapped('tracking_value_ids'):
+                for field in ['check_in', 'check_out']:
+                    if tracking.field == field:
+                        d1 = tracking.value_datetime
+                        d2 = tracking.message_id.date
+                        if (
+                            not d1
+                            or d1 < d2 - timedelta(seconds=60)
+                            or d1 > d2 + timedelta(seconds=60)
+                        ):
+                            self.time_changed_manually = True
