4ce7218580019923a1b88daa39e8989c3d8bf3aa
Alba Riera <alba.riera@creublanca.es>
2021-06-10 10:56:38 +0200

    [IMP] hr_employee_ppe: small changes

--- hr_employee_ppe/models/__init__.py
+++ hr_employee_ppe/models/__init__.py
@@ -1,2 +1,3 @@
 from . import hr_employee_material
 from . import product_template
+from . import hr_employee_material_request
--- hr_employee_ppe/models/hr_employee_material.py
+++ hr_employee_ppe/models/hr_employee_material.py
@@ -3,6 +3,7 @@
 
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.addons.base.models.ir_cron import _intervalTypes
 from datetime import date
 
 
@@ -12,57 +13,38 @@ class HrEmployeeMaterial(models.Model):
     _inherit = ["hr.employee.material"]
 
     is_ppe = fields.Boolean()
-    #start_date = fields.Date(string="Start Date", default=fields.Date.today())
     end_date = fields.Date()
-    description = fields.Text()
-    #partner_id = fields.Many2one(comodel_name="res.partner", string="Issued By", help="Certification Authority")
     indications = fields.Text(
         help="Situations in which the employee should use this equipment.",
     )
-    expire = fields.Boolean(help="True if the PPE expires")
+    expire_ppe = fields.Boolean(help="True if the PPE expires")
     certification = fields.Char(
         string="Certification Number", help="Certification Number"
     )
     issued_by = fields.Many2one(comodel_name="res.users")
-    """
-    status = fields.Selection(
-        [("valid", "Valid"), ("expired", "Expired")],
-        compute="_compute_status",
-        readonly=True,
-        help="PPE Status",
-    )
-    """
 
     @api.onchange('product_id')
     def _compute_fields(self):
         for rec in self:
             if rec.product_id.is_ppe:
                 rec.is_ppe = rec.product_id.is_ppe
-                if rec.product_id.expirable:
-                    rec.expire = rec.product_id.expirable
+                if rec.product_id.expirable_ppe:
+                    rec.expire_ppe = rec.product_id.expirable_ppe
                 if rec.product_id.indications:
                     rec.indications = rec.product_id.indications
-                if rec.product_id.description:
-                    rec.description = rec.product_id.description
-                if rec.product_id.certification:
-                    rec.certification = rec.product_id.certification
 
     def _validate_allocation_vals(self):
         res = super()._validate_allocation_vals()
         res['issued_by'] = self.env.user.id
+        if not self.end_date:
+            res["end_date"] = fields.Date.today() + _intervalTypes[
+                self.product_id.ppe_interval_type](self.product_id.ppe_duration)
         return res
 
     def validate_allocation(self):
-        self._check_dates()
         super().validate_allocation()
-    """
-    @api.depends("end_date", "start_date")
-    def _compute_state(self):
-        for rec in self:
-            if rec.state == "valid" and rec.expire and rec.end_date:
-                if rec.end_date < fields.Date.today():
-                    rec.status = "expired"
-    """
+        self._check_dates()
+
     @api.model
     def cron_ppe_expiry_verification(self, date_ref=None):
         if not date_ref:
@@ -75,21 +57,10 @@ class HrEmployeeMaterial(models.Model):
 
     def _check_dates(self):
         for record in self:
-            if record.expire:
-                if not record.end_date:
-                    raise ValidationError(
-                        _(
-                            """You must inform start date and
-                            end date for expirable PPEs."""
-                        )
-                    )
+            if record.expire_ppe:
                 start_date = record.start_date if record.start_date else date.today()
                 if record.end_date < start_date:
                     raise ValidationError(
                         _("End date cannot occur earlier than start date.")
                     )
 
-    def action_view_ppe_report(self):
-        report = self.env['ir.actions.report']._get_report_from_name(
-            'hr_employee_ppe.hr_employee_ppe_report_template')
-        return report.report_action(self)
