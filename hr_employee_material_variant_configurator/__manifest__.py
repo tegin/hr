526faa9f7a53efe0242201374f38de0c3c58e347
Alba Riera <alba.riera@creublanca.es>
2021-06-15 15:15:02 +0200

    [IMP] hr_employee_material_variant_configurator: add a module to gestionate product variants

---
+++ hr_employee_material_variant_configurator/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Cb Hr Employee Material',
+    'description': """
+        Manage employee material""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca',
+    'website': 'www.creublanca.es',
+    'depends': ['hr_employee_material', 'product_variant_configurator'],
+    'data': [
+        'views/hr_employee_material_request.xml',
+    ],
+    'demo': [
+    ],
+}
