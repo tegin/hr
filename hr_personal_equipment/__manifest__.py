8d69a6d4e9a38d3b9cfb44585ca5d10bc427e290
Alba Riera <alba.riera@creublanca.es>
2021-06-17 12:59:19 +0200

    [ADD] hr_personal_equipment: added module to manage employee personal equipment

---
+++ hr_personal_equipment/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hr Personal Equipment',
+    'summary': """
+        This addon allows to manage employee personal equipment""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Creu Blanca,Odoo Community Association (OCA)',
+    'website': 'www.creublanca.es',
+    'depends': ["product", "hr", "mail"],
+    'data': [
+        'security/hr_personal_equipment_security.xml',
+        'security/ir.model.access.csv',
+        'views/product_template.xml',
+        'views/hr_personal_equipment.xml',
+        'views/hr_personal_equipment_request.xml',
+    ],
+    'demo': [],
+}
