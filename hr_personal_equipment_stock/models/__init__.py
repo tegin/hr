4d604957112095e0f90e0385c49a60029220b337
Alba Riera <alba.riera@creublanca.es>
2021-06-18 08:39:56 +0200

    [IMP] hr_personal_equipment_stock: change name hr_employee_material to hr_personal_equipment

---
+++ hr_personal_equipment_stock/models/__init__.py
@@ -0,0 +1,6 @@
+from . import hr_personal_equipment_request
+from . import hr_personal_equipment
+from . import procurement_group
+from . import stock_picking
+from . import stock_move
+from . import stock_rule
